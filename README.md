4a08eefce902c6391578ee3eb4077de22690b08b
HooriaHIC <hooria.hic@gmail.com>
2022-10-27 07:28:25 +0500

    fixed files

---
+++ README.md
@@ -0,0 +1,11 @@
+## Getting Started
+
+Cmds:
+
+```bash
+npm install
+npm start
+
+# and to isntall and run the frontend app
+npm run client-build
+```
--- client/src/apis/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const baseURL =
-	process.env.NODE_ENV === "production" ? "https://quizerer.herokuapp.com" : "http://localhost:3000";
-
-export default axios.create({
-	baseURL,
-	headers: { "Content-Type": "application/json" }
-});
